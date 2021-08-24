fd9ecf8c6a9895ff1144537cab7e15127ed0cf5d
ishantchauhan1 <85096585+ishantchauhan1@users.noreply.github.com>
2021-08-24 13:07:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main 
+{
+    
+    public static void main(String[] args)
+    
+   { int a=10;
+    int b=5;
+    
+    System.out.println("Basic arthematic Expresions");
+    int c=a+b;
+    System.out.println("Sum is :"+c);
+    
+    int s=a-b;
+    System.out.println("Difference is :"+s);
+    
+    int m=a*b;
+    System.out.println("Multiply is :"+m);
+    
+    int d=a/b;
+    System.out.println("division is :"+d);
+    
+    
+    
+}}
